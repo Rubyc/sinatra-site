100060667a0f8aeb2662f6ae19c6747d872c848c
Hector <hector.bustillos@crowdint.com>
2011-03-22 22:28:26 -0600

    90% del placeholder listo

--- config/compass.rb
+++ config/compass.rb
@@ -4,14 +4,14 @@ if defined?(Sinatra)
   environment = :development
 else
   # this is the configuration to use when running within the compass command line tool.
-  css_dir = File.join 'static', 'stylesheets'
+  css_dir = File.join 'public', 'stylesheets'
   relative_assets = true
   environment = :production
 end
 
 # This is common configuration
 sass_dir = File.join 'views', 'stylesheets'
-images_dir = File.join 'static', 'images'
+images_dir = File.join 'public', 'images'
 http_path = "/"
 http_images_path = "/images"
 http_stylesheets_path = "/stylesheets"
